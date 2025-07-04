cb3039fac98ea6fb574cb4993ca6b45a6716963b
Mohit Singh Gurjar <mohitsinghgurjarofficial@gmail.com>
2025-07-04 20:50:51 +0530

    Java Basic To Advance

---
+++ Java Basic to Advanced/Test.java
@@ -0,0 +1,30 @@
+// this is single line comment
+// public class Test {
+
+//     public static void main(String[] args) {
+//         System.out.println("Hello World  !");
+//     }
+// }
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        byte a=30;
+        short b=244;
+
+        int age=34;
+        long d=24232342;
+        String name="Mohit";
+        float f=34.3f;
+        double g=2342.2322d;
+
+        char ch='A';
+
+        boolean bool=true;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(f);
+    }
+}
+
